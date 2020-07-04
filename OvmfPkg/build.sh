43cbaf52a68b3e4b0ecad44537f3b872cad0c556
Liu Yi <efikarl@icloud.com>
2020-07-04 22:50:36 +0800

    re-code ovmf build.sh script
    Signed-off-by: Liu Yi <efikarl@icloud.com>

--- OvmfPkg/build.sh
+++ OvmfPkg/build.sh
@@ -1,270 +1,183 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2008 - 2009, Apple Inc. All rights reserved.<BR>
-# Copyright (c) 2010 - 2019, Intel Corporation. All rights reserved.<BR>
+#  Copyright ©2020 efikarl, https://efikarl.com.
 #
-# SPDX-License-Identifier: BSD-2-Clause-Patent
+#  This program is just made available under the terms and conditions of the
+#  MIT license: https://efikarl.com/mit-license.html.
 #
-
+#  THE PROGRAM IS DISTRIBUTED UNDER THE MIT LICENSE ON AN "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+#
+#  A script to build OVMF.
 set -e
 shopt -s nocasematch
 
-
+THE_SCRIPT=${BASH_SOURCE:-$0}
+SCRIPT_DIR=$(dirname ${THE_SCRIPT})
 #
 # Setup workspace if it is not set
 #
-if [ -z "$WORKSPACE" ]
+args=$@
+if [ -z $WORKSPACE ] || [ -z $EDK_TOOLS_PATH ]
 then
-  echo Initializing workspace
-  if [ ! -e `pwd`/edksetup.sh ]
+  echo Setup workspace ...
+  if [ -f $SCRIPT_DIR/../edksetup.sh ]
   then
-    cd ..
-  fi
-# This version is for the tools in the BaseTools project.
-# this assumes svn pulls have the same root dir
-#  export EDK_TOOLS_PATH=`pwd`/../BaseTools
-# This version is for the tools source in edk2
-  export EDK_TOOLS_PATH=`pwd`/BaseTools
-  echo $EDK_TOOLS_PATH
-  source edksetup.sh BaseTools
-else
-  echo Building from: $WORKSPACE
-fi
-
-#
-# Configure defaults for various options
-#
-
-ARCH_IA32=no
-ARCH_X64=no
-BUILDTARGET=DEBUG
-BUILD_OPTIONS=
-PLATFORMFILE=
-THREADNUMBER=0
-LAST_ARG=
-RUN_QEMU=no
-ENABLE_FLASH=no
-
-#
-# Pick a default tool type for a given OS
-#
-TARGET_TOOLS=MYTOOLS
-case `uname` in
-  CYGWIN*)
-    echo Cygwin not fully supported yet.
-    ;;
-  Darwin*)
-    Major=$(uname -r | cut -f 1 -d '.')
-    # Major is Darwin version, not OS X version.
-    # OS X Yosemite 10.10.2 returns 14.
-    case $Major in
-      [156789])
-        echo OvmfPkg requires OS X Snow Leopard 10.6 or newer OS
-        exit 1
-        ;;
-      10)
-        TARGET_TOOLS=XCODE32
-        ;;
-      1[12])
-        TARGET_TOOLS=XCLANG
-        ;;
-       *)
-        # Mavericks and future assume XCODE5 (clang + lldb)
-        TARGET_TOOLS=XCODE5
-        ;;
-    esac
-    ;;
-  Linux*)
-    gcc_version=$(gcc -v 2>&1 | tail -1 | awk '{print $3}')
-    case $gcc_version in
-      [1-3].*|4.[0-7].*)
-        echo OvmfPkg requires GCC4.8 or later
-        exit 1
-        ;;
-      4.8.*)
-        TARGET_TOOLS=GCC48
-        ;;
-      4.9.*|6.[0-2].*)
-        TARGET_TOOLS=GCC49
-        ;;
-      *)
-        TARGET_TOOLS=GCC5
-        ;;
-    esac
-esac
-
-#
-# Scan command line to override defaults
-#
-
-for arg in "$@"
-do
-  if [ -z "$LAST_ARG" ]; then
-    case $arg in
-      -a|-b|-t|-p|-n)
-        LAST_ARG=$arg
-        ;;
-      qemu)
-        RUN_QEMU=yes
-        shift
-        break
-        ;;
-      --enable-flash)
-        ENABLE_FLASH=yes
-        ;;
-      *)
-        BUILD_OPTIONS="$BUILD_OPTIONS $arg"
-        ;;
-    esac
+    set --
+    source $SCRIPT_DIR/../edksetup.sh
+    set -- $args
   else
-    case $LAST_ARG in
-      -a)
-        if [[ x"$arg" != x"IA32" && x"$arg" != x"X64" ]]; then
-          echo Unsupported processor architecture: $arg
-          echo Only IA32 or X64 is supported
-          exit 1
-        fi
-        eval ARCH_$arg=yes
-        ;;
-      -b)
-        BUILDTARGET=$arg
-        ;;
-      -p)
-        PLATFORMFILE=$arg
-        ;;
-      -t)
-        TARGET_TOOLS=$arg
-        ;;
-      -n)
-        THREADNUMBER=$arg
-        ;;
-      *)
-        BUILD_OPTIONS="$BUILD_OPTIONS $arg"
-        ;;
-    esac
-    LAST_ARG=
+    echo "[ERR]:$THE_SCRIPT: no edksetup.sh found in \$WORKSPACE=$WORKSPACE!"
+    exit 1
   fi
-  shift
-done
-
-if [[ "$ARCH_IA32" == "yes" && "$ARCH_X64" == "yes" ]]; then
-  PROCESSOR=IA32X64
-  Processor=Ia32X64
-  BUILD_OPTIONS="$BUILD_OPTIONS -a IA32 -a X64"
-  PLATFORM_BUILD_DIR=Ovmf3264
-  BUILD_ROOT_ARCH=X64
-elif [[ "$ARCH_IA32" == "yes" && "$ARCH_X64" == "no" ]]; then
-  PROCESSOR=IA32
-  Processor=Ia32
-  BUILD_OPTIONS="$BUILD_OPTIONS -a IA32"
-  PLATFORM_BUILD_DIR=Ovmf$Processor
-  BUILD_ROOT_ARCH=$PROCESSOR
-else
-  PROCESSOR=X64
-  Processor=X64
-  BUILD_OPTIONS="$BUILD_OPTIONS -a X64"
-  PLATFORM_BUILD_DIR=Ovmf$Processor
-  BUILD_ROOT_ARCH=X64
 fi
-
-case $PROCESSOR in
+#
+# Filter out _BUILD_ARGS_ and _QEMUx_ARGS_
+#
+while [[ $# > 0 ]]
+do
+  case $1 in
+  -p | -a | -b | -t | -n)
+    _BUILD_ARGS_="$_BUILD_ARGS_ $1 $2"
+    shift 2
+    ;;
+  *)
+    _QEMUx_ARGS_="$_QEMUx_ARGS_ $1"
+    shift 1
+    ;;
+  esac
+done
+#
+# _BUILD_ARGS_ parsing to override rules in target.txt
+#
+set -- $_BUILD_ARGS_
+while getopts a:b:p:t:n: arg
+do
+  case $arg in
+  p)
+    BUILD_PLAT=$OPTARG;;
+  a)
+    BUILD_ARGS="$BUILD_ARGS -a $OPTARG" && BUILD_ARCH=$BUILD_ARCH$OPTARG;;
+  b)
+    BUILD_ARGS="$BUILD_ARGS -b $OPTARG" && BUILD_TYPE=$OPTARG;;
+  t)
+    BUILD_ARGS="$BUILD_ARGS -t $OPTARG" && BUILD_TOOL=$OPTARG;;
+  n)
+    BUILD_ARGS="$BUILD_ARGS -n $OPTARG" && THREAD_NUM=$OPTARG;;
+  ?)
+    echo "[ERR]:$THE_SCRIPT: invalid build option: $arg!"
+    exit 1
+    ;;
+  esac
+done
+#
+# Setup default build arguments that not given
+#
+case $BUILD_ARCH in
   IA32)
-    if [ -n "$QEMU_COMMAND" ]; then
-      #
-      # The user set the QEMU_COMMAND variable. We'll use it to run QEMU.
-      #
-      :
-    elif  [ -x `which qemu-system-i386` ]; then
-      QEMU_COMMAND=qemu-system-i386
-    elif  [ -x `which qemu-system-x86_64` ]; then
-      QEMU_COMMAND=qemu-system-x86_64
-    elif  [ -x `which qemu` ]; then
-      QEMU_COMMAND=qemu
+    if [[ -z $BUILD_PLAT ]]
+    then
+      BUILD_PLAT=OvmfPkg/OvmfPkgIa32.dsc
+    fi
+    PLATFORM_BUILD_DIR=OvmfIa32
+    ;;
+  X64)
+    if [[ -z $BUILD_PLAT ]]
+    then
+      BUILD_PLAT=OvmfPkg/OvmfPkgX64.dsc
+    fi
+    PLATFORM_BUILD_DIR=OvmfX64
+    ;;
+  IA32X64 | X64IA32)
+    if [[ -z $BUILD_PLAT ]]
+    then
+      BUILD_PLAT=OvmfPkg/OvmfPkgIa32X64.dsc
+    fi
+    PLATFORM_BUILD_DIR=Ovmf3264
+    ;;
+  *)
+    if [[ -z $BUILD_ARCH ]]
+    then
+      BUILD_PLAT=OvmfPkg/OvmfPkgIa32X64.dsc
+      PLATFORM_BUILD_DIR=Ovmf3264
     else
-      echo Unable to find QEMU for IA32 architecture!
+      echo "[ERR]:$THE_SCRIPT: invaild build arch till now: $BUILD_ARCH!"
       exit 1
     fi
     ;;
-  X64|IA32X64)
-    if [ -z "$QEMU_COMMAND" ]; then
-      #
-      # The user didn't set the QEMU_COMMAND variable.
-      #
-      QEMU_COMMAND=qemu-system-x86_64
-    fi
+esac
+  BUILD_ARGS="$BUILD_ARGS -p $BUILD_PLAT"
+[[ -z $BUILD_ARCH ]] && {
+  BUILD_ARCH=X64
+  BUILD_ARGS="$BUILD_ARGS -a IA32 -a X64"
+}
+[[ -z $BUILD_TYPE ]] && {
+  BUILD_TYPE=DEBUG
+  BUILD_ARGS="$BUILD_ARGS -b $BUILD_TYPE"
+}
+[[ -z $BUILD_TOOL ]] && {
+  case `uname` in
+  Darwin*)
+    BUILD_TOOL=XCODE5;;
+  Linux*)
+    BUILD_TOOL=GCC5;;
+  esac
+  BUILD_ARGS="$BUILD_ARGS -t $BUILD_TOOL"
+}
+  BUILD_ARGS="$BUILD_ARGS -j $WORKSPACE/build.log"
+#
+# _QEMUx_ARGS_ parsing
+#
+set -- $_QEMUx_ARGS_
+while [[ $# > 0 ]]
+do
+  case $1 in
+  qemu)
+    RUN_QEMU=RunGumpRun
+    shift 1
     ;;
   *)
-    echo Unsupported processor architecture: $PROCESSOR
-    echo Only IA32 or X64 is supported
+    QEMUx_ARGS="$QEMUx_ARGS $1"
+    shift 1
+    ;;
+  esac
+done
+#
+# Build or Run OVMF
+#
+if [[ $RUN_QEMU != RunGumpRun ]]
+then
+  echo build $BUILD_ARGS && build $BUILD_ARGS
+else
+  case $BUILD_ARCH in
+  IA32)
+    QEMU_CMD=qemu-system-i386
+    ;;
+  X64 | IA32X64 | X64IA32)
+    QEMU_CMD=qemu-system-x86_64
+    ;;
+  *)
+    echo "[ERR]:$THE_SCRIPT: unsupported cpu architecture: $OPTARG!"
     exit 1
     ;;
-esac
-
-if [ -z "$PLATFORMFILE" ]; then
-  PLATFORMFILE=$WORKSPACE/OvmfPkg/OvmfPkg$Processor.dsc
-fi
-
-if [[ "$RUN_QEMU" == "yes" ]]; then
-  qemu_version=$($QEMU_COMMAND -version 2>&1 | \
-                   grep -o -E 'version [0-9]+\.[0-9]+\.[0-9]+' | \
-                     awk '{print $2}')
-  case $qemu_version in
-    1.[6-9].*|[2-9].*.*|[1-9][0-9]*.*.*)
-      ENABLE_FLASH=yes
-      ;;
   esac
-fi
-
-#
-# Uncomment this block for parameter parsing debug
-#
-#echo RUN_QEMU=$RUN_QEMU
-#echo BUILD_OPTIONS=$BUILD_OPTIONS
-#echo BUILDTARGET=$BUILDTARGET
-#echo TARGET_TOOLS=$TARGET_TOOLS
-#echo PROCESSOR=$PROCESSOR
-#echo Remaining for qemu: $*
-#exit 1
-
-BUILD_ROOT=$WORKSPACE/Build/$PLATFORM_BUILD_DIR/"$BUILDTARGET"_"$TARGET_TOOLS"
-FV_DIR=$BUILD_ROOT/FV
-BUILD_ROOT_ARCH=$BUILD_ROOT/$BUILD_ROOT_ARCH
-QEMU_FIRMWARE_DIR=$BUILD_ROOT/QEMU
-
-if  [[ ! -f `which build` || ! -f `which GenFv` ]];
-then
-  # build the tools if they don't yet exist. Bin scheme
-  echo Building tools as they are not in the path
-  make -C $WORKSPACE/BaseTools
-elif [[ ( -f `which build` ||  -f `which GenFv` )  && ! -d  $EDK_TOOLS_PATH/Source/C/bin ]];
-then
-  # build the tools if they don't yet exist. BinWrapper scheme
-  echo Building tools no $EDK_TOOLS_PATH/Source/C/bin directory
-  make -C $WORKSPACE/BaseTools
-else
-  echo using prebuilt tools
-fi
-
-
-if [[ "$RUN_QEMU" == "yes" ]]; then
-  if [[ ! -d $QEMU_FIRMWARE_DIR ]]; then
-    mkdir $QEMU_FIRMWARE_DIR
+  if ! ( command -v $QEMU_CMD >/dev/null 2>&1 )
+  then
+    echo "[ERR]:$THE_SCRIPT: qemu is not found!"
+    exit 1
   fi
-  ln -sf $FV_DIR/OVMF.fd $QEMU_FIRMWARE_DIR/bios.bin
-  if [[ "$ENABLE_FLASH" == "yes" ]]; then
-    QEMU_COMMAND="$QEMU_COMMAND -drive if=pflash,format=raw,file=$QEMU_FIRMWARE_DIR/bios.bin"
-  else
-    QEMU_COMMAND="$QEMU_COMMAND -L $QEMU_FIRMWARE_DIR"
-  fi
-  echo Running: $QEMU_COMMAND "$@"
-  $QEMU_COMMAND "$@"
-  exit $?
+
+  OVMF_FD=$WORKSPACE/Build/$PLATFORM_BUILD_DIR/"$BUILD_TYPE"_"$BUILD_TOOL"/FV/OVMF.fd
+  [[ -f $OVMF_FD ]] || build $BUILD_ARGS
+  case $QEMU_VER in
+  0.*.* | 1.[0-5].*)
+    QEMUx_ARGS="$QEMUx_ARGS -bios $OVMF_FD"
+    ;;
+  *)
+    QEMUx_ARGS="$QEMUx_ARGS -drive if=pflash,format=raw,file=$OVMF_FD"
+    ;;
+  esac
+
+  QEMU_DBG="-debugcon file:debug.log -global isa-debugcon.iobase=0x402"
+  echo "Running: $QEMU_CMD $QEMUx_ARGS" && cd $WORKSPACE && $QEMU_CMD $QEMU_DBG $QEMUx_ARGS
 fi
-
-#
-# Build the edk2 OvmfPkg
-#
-echo Running edk2 build for OvmfPkg$Processor
-build -p $PLATFORMFILE $BUILD_OPTIONS -b $BUILDTARGET -t $TARGET_TOOLS -n $THREADNUMBER
-exit $?
-
